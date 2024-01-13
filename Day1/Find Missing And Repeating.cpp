eb0bd7c8f1f8e40ba5d9401996ad7b73a72fd016
Navraj Devali <110734072+Navraj88@users.noreply.github.com>
2024-01-13 10:28:58 +0530

    Create Find Missing And Repeating.cpp

---
+++ Day1/Find Missing And Repeating.cpp
@@ -0,0 +1,26 @@
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+    int repeatingNumber = -1;
+        int missingNumber = -1;
+        
+        // Rearrange the array such that the element at each index is equal to its value
+        for (int i = 0; i < n; ) {
+            if (arr[i] != arr[arr[i] - 1]) {
+                swap(arr[i], arr[arr[i] - 1]);
+            } else {
+                i++;
+            }
+        }
+        
+        // Find the first mismatched element to identify the repeating and missing numbers
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                repeatingNumber = arr[i];
+                missingNumber = i + 1;
+                break;
+            }
+        }
+        
+        return {repeatingNumber, missingNumber};
+    }
