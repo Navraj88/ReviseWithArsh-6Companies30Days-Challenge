966af5c679ed7dbdd52ceec8d54c086f51c72642
Navraj Devali <110734072+Navraj88@users.noreply.github.com>
2024-01-18 16:17:32 +0530

    Create 661. Image Smoother.cpp

---
+++ company 2 microsoft/661. Image Smoother.cpp
@@ -0,0 +1,48 @@
+/*
+661. Image Smoother
+Solved
+Easy
+Topics
+Companies
+An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        // Save the dimensions of the image.
+        int m = img.size();
+        int n = img[0].size();
+
+        // Create a new image of the same dimension as the input image.
+        vector<vector<int>> smoothImg(m, vector<int>(n));
+
+        // Iterate over the cells of the image.
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Initialize the sum and count 
+                int sum = 0;
+                int count = 0;
+
+                // Iterate over all plausible nine indices.
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        // If the indices form valid neighbor
+                        if (0 <= x && x < m && 0 <= y && y < n) {
+                            sum += img[x][y];
+                            count += 1;
+                        }
+                    }
+                }
+
+                // Store the rounded down value in smoothImg[i][j].
+                smoothImg[i][j] = sum / count;
+            }
+        }
+
+        // Return the smooth image.
+        return smoothImg;
+    }
+};
